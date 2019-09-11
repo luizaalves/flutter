2fd3aa042e83ba214d02b4c9d708525e3699c7c5
Luiza <l.uhzinha@hotmail.com>
2019-09-11 16:56:13 -0300

    Testes unitários

---
+++ myapp/lib/Counter.dart
@@ -0,0 +1,10 @@
+class Counter {
+  /**
+   * Classe a ser testada
+   */
+  int value = 1;
+
+  void increment() => value++;
+
+  void decrement() => value--;
+}
---
+++ myapp/test/counter_test.dart
@@ -0,0 +1,37 @@
+// Import the test package and Counter class
+import 'package:test/test.dart';
+import 'package:myapp/Counter.dart'; //caminho da classe a ser testada
+
+void main() {
+  /* Teste unitário
+  test('Counter value should be incremented', () {
+    final counter = Counter();
+
+    counter.increment();
+
+    expect(counter.value, 2);
+  });
+  */
+  //Grupo com vários testes em separado
+  group('Counter', () {
+    test('value should start at 1', () {
+      expect(Counter().value, 1);
+    });
+
+    test('value should be incremented', () {
+      final counter = Counter();
+
+      counter.increment();
+
+      expect(counter.value, 2);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, 0);
+    });
+  });
+}
